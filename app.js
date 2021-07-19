45ffbde89db1e9630b81ce03103b548e08eaf66b
pancake <leomassfelder@outlook.fr>
2021-07-19 17:08:09 +0200

    Basic calculator working

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const container = document.getElementById("container");
 const screen = document.getElementById("screen");
+const operatorBtn = document.querySelectorAll(".operatorBtn");
+const numberBtn = document.querySelectorAll(".numberBtn");
 const num0Btn = document.getElementById("num0");
 const num1Btn = document.getElementById("num1");
 const num2Btn = document.getElementById("num2");
@@ -19,20 +21,59 @@ const subtractBtn = document.getElementById("subtract");
 const addBtn = document.getElementById("add");
 const dotBtn = document.getElementById("dot");
 
+let displayValue = 0;
+
+function screenUpdate() {
+	screen.innerText += displayValue;
+}
+
+function displayNum() {
+	numberBtn.forEach(
+		(e) =>
+			(e.onclick = () => {
+				displayValue = e.innerText;
+				screenUpdate();
+			})
+	);
+}
+displayNum();
+
+let sum;
+let savedValue;
+let selectedOperator;
+
+operatorBtn.forEach((btn) => {
+	btn.onclick = () => {
+		savedValue = screen.innerText;
+		selectedOperator = btn.innerText;
+		screen.innerText = "";
+	};
+});
+
+equalBtn.onclick = () => {
+	let a = parseInt(savedValue);
+	let b = parseInt(screen.innerText);
+	screen.innerText = operate(selectedOperator, a, b);
+};
+
 function add(a, b) {
-	return a + b;
+	sum = a + b;
+	return sum;
 }
 
 function subtract(a, b) {
-	return a - b;
+	sum = a - b;
+	return sum;
 }
 
 function multiply(a, b) {
-	return a * b;
+	sum = a * b;
+	return sum;
 }
 
 function divide(a, b) {
-	return a / b;
+	sum = a / b;
+	return sum;
 }
 
 function operate(operator, a, b) {
@@ -51,3 +92,10 @@ function operate(operator, a, b) {
 			break;
 	}
 }
+
+acBtn.onclick = function() {
+	sum = 0;
+	savedValue = "";
+	selectedOperator = "";
+	screen.innerText = "";
+};
